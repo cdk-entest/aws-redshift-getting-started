18075941c36c0f3916cffedd006d847fe423732d
entest <hai@entest.io>
2023-01-17 01:16:45 +0000

    Initial commit

---
+++ test/redshift-serverless-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RedshiftServerlessDemo from '../lib/redshift-serverless-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/redshift-serverless-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RedshiftServerlessDemo.RedshiftServerlessDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
